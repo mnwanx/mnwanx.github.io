530149fe7de821f196af3a1dc72290ca25671971
Muhammad Najwan <najwanhassan03@gmail.com>
2025-01-30 02:17:24 +0800

    Installer update

--- streameo_desktop_installer/main.cpp
+++ streameo_desktop_installer/main.cpp
@@ -7,20 +7,29 @@ using namespace std;
 const string base_link = "https://github.com/mnwanx/mnwanx.github.io/raw/refs/heads/master/streameo_desktop_release/";
 bool downloader(int part)   {
     if (part < 10)  {
-        string command = "curl -L " + base_link + "Streameo.part0" + to_string(part) + ".rar -o Streameo.part0" + to_string(part) + ".rar";
+        string command = "curl -s -L " + base_link + "Streameo.part0" + to_string(part) + ".rar -o Streameo.part0" + to_string(part) + ".rar";
         system(command.c_str());
-        cout << "Streameo.part0" << part << ".rar" << endl;
+        
     }   else    {
-        string command = "curl -L " + base_link + "Streameo.part" + to_string(part) + ".rar -o Streameo.part" + to_string(part) + ".rar";
+        string command = "curl -s -L " + base_link + "Streameo.part" + to_string(part) + ".rar -o Streameo.part" + to_string(part) + ".rar";
         system(command.c_str());
-        cout << "Streameo.part" << part << ".rar" << endl;
+        
     }
     return true;
 }
 
 int main()  {
+    system("echo off");
     for (int i = 1; i <= 21; i++)   {
         downloader(i);
+        float percentage = i / 21.0 * 100;
+        if (i < 10) {
+            cout << "[ " <<  int(percentage) <<  "% ] Streameo.part0" << i << ".rar" << endl;
+        }   else    {
+            cout << "[ " << int(percentage) <<  "% ] Streameo.part" << i << ".rar" << endl;
+        }
     }
+    system("echo on && msg * Done! Combine all parts with WinRAR and enjoy!");
+    
     return 0;
 }
