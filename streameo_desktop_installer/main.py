0b6463438b641f24164c7116697668b38c693f84
Muhammad Najwan <najwanhassan03@gmail.com>
2025-05-25 01:49:53 +0800

    Streameo Installer Update

---
+++ streameo_desktop_installer/main.py
@@ -0,0 +1,45 @@
+from os import system
+from threading import Thread
+
+base_link = "https://github.com/mnwanx/mnwanx.github.io/raw/refs/heads/master/streameo_desktop_release/"
+
+complete_part = 0
+
+def downloader(part):
+    global complete_part
+    if part < 10:
+        command = "curl -s -L " + base_link + "Streameo.part0" + str(part) + ".rar -o Streameo.part0" + str(part) + ".rar"
+        system(command)
+        
+    else:    
+        command = "curl -s -L " + base_link + "Streameo.part" + str(part) + ".rar -o Streameo.part" + str(part) + ".rar"
+        system(command)
+        
+    complete_part += 1
+    updater()
+
+
+banner = """
+   _____ _                                         _____           _        _ _           
+  / ____| |                                       |_   _|         | |      | | |          
+ | (___ | |_ _ __ ___  __ _ _ __ ___   ___  ___     | |  _ __  ___| |_ __ _| | | ___ _ __ 
+  \___ \| __| '__/ _ \/ _` | '_ ` _ \ / _ \/ _ \    | | | '_ \/ __| __/ _` | | |/ _ | '__|
+  ____) | |_| | |  __| (_| | | | | | |  __| (_) |  _| |_| | | \__ | || (_| | | |  __| |   
+ |_____/ \__|_|  \___|\__,_|_| |_| |_|\___|\___/  |_____|_| |_|___/\__\__,_|_|_|\___|_|
+ v2.0
+
+Project Name: Streameo Desktop
+
+Visit Streameo Project: https://streameo.onrender.com
+Devepoed by Code                                                                                          
+
+"""
+
+system('cls')
+print(banner)
+for i in range(1, 22):
+    Thread(target=downloader, args={i,}).start()
+
+def updater():
+    print(f"Downloading: {round(complete_part / 21.0 * 100, 1)} % | {complete_part} / 21", end="\r")
+updater()
