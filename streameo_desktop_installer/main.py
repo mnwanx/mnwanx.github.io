40a53e9c1a81032da96c96b0f3a2fa7efd474df4
Muhammad Najwan <najwanhassan03@gmail.com>
2025-07-04 10:32:56 +0800

    update

--- streameo_desktop_installer/main.py
+++ streameo_desktop_installer/main.py
@@ -1,4 +1,4 @@
-from os import system
+from os import system, mkdir, chdir, listdir, remove
 from threading import Thread
 
 base_link = "https://github.com/mnwanx/mnwanx.github.io/raw/refs/heads/master/streameo_desktop_release/"
@@ -26,7 +26,7 @@ banner = """
   \___ \| __| '__/ _ \/ _` | '_ ` _ \ / _ \/ _ \    | | | '_ \/ __| __/ _` | | |/ _ | '__|
   ____) | |_| | |  __| (_| | | | | | |  __| (_) |  _| |_| | | \__ | || (_| | | |  __| |   
  |_____/ \__|_|  \___|\__,_|_| |_| |_|\___|\___/  |_____|_| |_|___/\__\__,_|_|_|\___|_|
- v2.0
+ v3.0
 
 Project Name: Streameo Desktop
 
@@ -37,9 +37,34 @@ Devepoed by Code
 
 system('cls')
 print(banner)
+try:
+    mkdir("StreameoDesktop")
+except:
+    pass
+chdir("StreameoDesktop")
 for i in range(1, 22):
     Thread(target=downloader, args={i,}).start()
 
 def updater():
     print(f"Downloading: {round(complete_part / 21.0 * 100, 1)} % | {complete_part} / 21", end="\r")
 updater()
+
+
+while True:
+    if complete_part == 21:
+        all_part = listdir()
+
+        with open("Streameo.rar", "wb") as file:
+            for i in range(len(all_part)):
+                with open(all_part[i], 'rb') as file2:
+                    file.write(file2.read())
+                    print(f"Compiling {i / len(all_part) * 100} %", end="\r")
+                    file2.close()
+                remove(all_part[i])
+            file.close()
+
+
+        system('msg * Compilation done! Extract \"StreameoDesktop/Streameo.rar\" file')
+        break
+    else:
+        pass
