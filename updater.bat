454369c16a1b28167fa25b2ba118e363c9c204dd
Muhammad Najwan <najwanhassan03@gmail.com>
2025-10-01 19:26:29 +0800

    database update

--- updater.bat
+++ updater.bat
@@ -1,11 +1,11 @@
 @echo off
 set /p update_msg="Enter commit msg >> "
-set msg = %update_msg%
 
 git add .
-IF "%msg%"=="" (
+IF "%update_msg%"=="" (
     git commit -m "Update"
 ) ELSE (
-    git commit -m "%msg%"
+    git commit -m "%update_msg%"
 )
 git push
+
